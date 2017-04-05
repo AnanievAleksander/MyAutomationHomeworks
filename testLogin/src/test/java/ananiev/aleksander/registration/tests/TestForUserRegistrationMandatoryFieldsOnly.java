d9e837e0bd2169a798368bae80d038e485251550
Aleksander Ananiev <ananiev.aleksander@gmail.com>
2017-04-05 07:41:48 +0300

    "My first Eclipse Commit"

---
+++ testLogin/src/test/java/ananiev/aleksander/registration/tests/TestForUserRegistrationMandatoryFieldsOnly.java
@@ -0,0 +1,19 @@
+package ananiev.aleksander.registration.tests;
+
+import org.junit.Before;
+import org.junit.Test;
+
+import bg.pragmatic.myautomationframework.utils.Browser;
+
+public class TestForUserRegistrationMandatoryFieldsOnly {
+
+	@Before
+	public void init() {
+		Browser.setup();
+	}
+
+	@Test
+	public void correctUserData() {
+	}
+
+}
