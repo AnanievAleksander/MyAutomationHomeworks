51db8be59fbe6a2167677b7d92b0ba0f49e89abf
Aleksander Ananiev <ananiev.aleksander@gmail.com>
2017-08-06 11:04:53 +0300

    MyFirstAppium

---
+++ MyFirstAppiumTest/src/main/java/ananiev/alex/MyFirstAppiumTest/App.java
@@ -0,0 +1,13 @@
+package ananiev.alex.MyFirstAppiumTest;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
